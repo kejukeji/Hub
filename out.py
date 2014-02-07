2c3b52d6622373fb5775c1ad144c6d4d9406bb93
yuzhe0918 <xiangyuzhe0918@126.com>
2014-02-07 16:43:54 +0800

    旋转方向。

--- out.py
+++ out.py
@@ -2,7 +2,7 @@
 
 def out_image_name():
     '''输出奶粉图片名字'''
-    count = 0
+    count = 100
     while True:
         temp_count = str(count)
         temp_len = len(temp_count)
@@ -13,9 +13,9 @@ def out_image_name():
         if temp_len == 3:
             print "<img src='/static/images/milk/0"+ str(temp_count)+".png'>"
 
-        if count >= 100:
+        if count <= 0:
             break
-        count = count + 1
+        count = count - 1
 
 
 
